a709f9f0e986d52f9d9c1f5f0224e851c6a86df0
vineet-proptiger <vineet.mishra@proptiger.com>
2026-04-14 21:27:50 +0530

    some fixes

--- app/page.js
+++ app/page.js
@@ -55,7 +55,8 @@ export default function Home() {
       <div className="sticky-bottom-bar">
         <a
           href="tel:+919718344024"
-          className="flex-1 flex items-center justify-center btn-primary !py-2 !px-0 !rounded-none"
+          className="flex-1 flex items-center justify-center !py-2 !px-0 text-white transition-all"
+          style={{ background: '#111827' }}
         >
           <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
             <path d="M6.6 10.8c1.4 2.8 3.8 5.1 6.6 6.6l2.2-2.2c.3-.3.7-.4 1-.2 1.1.4 2.3.6 3.6.6.6 0 1 .4 1 1V20c0 .6-.4 1-1 1-9.4 0-17-7.6-17-17 0-.6.4-1 1-1h3.5c.6 0 1 .4 1 1 0 1.3.2 2.5.6 3.6.1.3 0 .7-.2 1L6.6 10.8z" />
